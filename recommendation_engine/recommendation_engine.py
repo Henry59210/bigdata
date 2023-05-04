a9a81ded8ea939d72a69f18cdd2993bbc52d911c
huangzhaoxin <1614629345@qq.com>
2023-05-04 16:26:48 +0800

    fix

--- recommendation_engine/recommendation_engine.py
+++ recommendation_engine/recommendation_engine.py
@@ -1,17 +1,14 @@
 from pyspark.shell import spark
 from pyspark.sql import SparkSession
 
+from hdfs_utils.load import load_topic
+
 # game detail data
 
 topicsList = ["game_detail", "user_owned_games", "user_friend_list", "user_recently_played_games", "user_idx",
               "user_summary"]
 
 if __name__ == '__main__':
-    sc = SparkSession \
-        .builder \
-        .appName("spark-recommender") \
-        .getOrCreate()
-
     for i in topicsList:
-        df = spark.read.json("hdfs://localhost:9000/topics/" + i + "/partition=0")
-        print(df)
+        result = load_topic(i)
+        print(result)
